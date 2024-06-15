b4d628623f25864203c2a4e7ebfc50ba293ca245
JEROLPOA2 <jledesmap@unal.edu.co>
2024-06-14 21:21:29 -0500

    Login implementation

---
+++ frontend/src/data/users.tsx
@@ -0,0 +1,32 @@
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    email: "john.doe@example.com",
+    password: "Password123!",
+    role: "entrenador",
+  },
+  {
+    firstName: "Jane",
+    lastName: "Smith",
+    email: "jane.smith@example.com",
+    password: "SecurePassword456!",
+    role: "enfermera",
+  },
+  {
+    firstName: "Carlos",
+    lastName: "Mendez",
+    email: "carlos.mendez@example.com",
+    password: "Carlos789#",
+    role: "entrenador",
+  },
+  {
+    firstName: "Maria",
+    lastName: "Gomez",
+    email: "maria.gomez@example.com",
+    password: "MariaSecure2023!",
+    role: "enfermera",
+  },
+];
+
+export default users;
