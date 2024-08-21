8822a6fd909a3170cbbc93e97a42a738fd2597ff
JEROLPOA2 <jledesmap@unal.edu.co>
2024-08-20 21:14:00 -0500

    Nurse interface update

--- frontend/src/elements/table-row/table-row.tsx
+++ frontend/src/elements/table-row/table-row.tsx
@@ -35,13 +35,14 @@ const TableRow: React.FC<TableRowProps> = ({ pokemones }) => {
     return turnsList.find((pokemon: Pokemon) => pokemon.turn === turno);
   };
 
-  const selectedPokemon = selectedTurnIndex !== null ? getPokemonByTurn(selectedTurnIndex) : null;
+  const selectedPokemon =
+    selectedTurnIndex !== null ? getPokemonByTurn(selectedTurnIndex) : null;
 
   const removeFirstPokemon = () => {
-    setTurnsList(prevTurnsList => {
+    setTurnsList((prevTurnsList) => {
       const updatedTurnsList = prevTurnsList.slice(1).map((turn, index) => ({
         ...turn,
-        turn: index + 1
+        turn: index + 1,
       }));
       return updatedTurnsList;
     });
@@ -49,81 +50,87 @@ const TableRow: React.FC<TableRowProps> = ({ pokemones }) => {
 
   return (
     <>
-    <div className="button-container">
-      <button 
-        className="btn btn-primary btn-md justify-content-center rounded-pill"
-        type="button"
-        onClick={() => removeFirstPokemon()}
-      >
-        Atender siguiente turno
-      </button>
-    </div>
-    <div className="grid-container-nurse">
-      <div className="d-flex justify-content-center item-pokemon mt-3" ref={cardRef}>
-        {selectedPokemon && (
-          <PokemonCard pokemon={selectedPokemon}/>
-        )}
+      <div className="d-flex-container-nurse">
+        <div
+          className="d-flex justify-content-center item-pokemon mt-3"
+          ref={cardRef}
+        >
+          {selectedPokemon && <PokemonCard pokemon={selectedPokemon} />}
+        </div>
+        <div className="table-turnos mt-3">
+          <DragDropContext onDragEnd={onDragEnd}>
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Turno</th>
+                  <th scope="col">Pokémon</th>
+                  <th scope="col">Nivel</th>
+                  <th scope="col">PV</th>
+                  <th scope="col">Estado Actual</th>
+                  <th scope="col">Entrenador</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <Droppable droppableId="tasks">
+                {(droppableProvider) => (
+                  <tbody
+                    {...droppableProvider.droppableProps}
+                    ref={droppableProvider.innerRef}
+                  >
+                    {turnsList.map((pokemon, index) => (
+                      <Draggable
+                        key={pokemon.turn}
+                        draggableId={pokemon.turn.toString()}
+                        index={index}
+                      >
+                        {(draggableProvider) => (
+                          <tr
+                            {...draggableProvider.draggableProps}
+                            {...draggableProvider.dragHandleProps}
+                            ref={draggableProvider.innerRef}
+                            className="tasks-item"
+                          >
+                            <td>{pokemon.turn}</td>
+                            <td>{pokemon.name}</td>
+                            <td>{pokemon.level}</td>
+                            <td>{pokemon.lifePoints}</td>
+                            <td>
+                              {pokemon.status
+                                .map((s: Status) => s.name)
+                                .join(", ")}
+                            </td>
+                            <td>{pokemon.trainer}</td>
+                            <td>
+                              <button
+                                className="btn btn-primary btn-sm rounded-pill"
+                                type="button"
+                                onClick={() => handleDetailsClick(pokemon.turn)}
+                              >
+                                Detalles
+                              </button>
+                            </td>
+                          </tr>
+                        )}
+                      </Draggable>
+                    ))}
+                    {droppableProvider.placeholder}
+                  </tbody>
+                )}
+              </Droppable>
+            </table>
+          </DragDropContext>
+        </div>
       </div>
-      <div className="table-turnos mt-3">
-        <DragDropContext onDragEnd={onDragEnd}>
-          <table className="table table-striped">
-            <thead>
-              <tr>
-                <th scope="col">Turno</th>
-                <th scope="col">Pokémon</th>
-                <th scope="col">Nivel</th>
-                <th scope="col">PV</th>
-                <th scope="col">Estado Actual</th>
-                <th scope="col">Entrenador</th>
-                <th scope="col"></th>
-              </tr>
-            </thead>
-            <Droppable droppableId="tasks">
-              {(droppableProvider) => (
-                <tbody
-                  {...droppableProvider.droppableProps}
-                  ref={droppableProvider.innerRef}
-                >
-                  {turnsList.map((pokemon, index) => (
-                    <Draggable
-                      key={pokemon.turn}
-                      draggableId={pokemon.turn.toString()}
-                      index={index}
-                    >
-                      {(draggableProvider) => (
-                        <tr
-                          {...draggableProvider.draggableProps}
-                          {...draggableProvider.dragHandleProps}
-                          ref={draggableProvider.innerRef}
-                          className="tasks-item"
-                        >
-                          <td>{pokemon.turn}</td>
-                          <td>{pokemon.name}</td>
-                          <td>{pokemon.level}</td>
-                          <td>{pokemon.lifePoints}</td>
-                          <td>{pokemon.status.map((s: Status) => s.name).join(', ')}</td>
-                          <td>{pokemon.trainer}</td>
-                          <td>
-                            <button
-                              className="btn btn-primary btn-sm rounded-pill"
-                              type="button"
-                              onClick={() => handleDetailsClick(pokemon.turn)}
-                            >
-                              Detalles
-                            </button>
-                          </td>
-                        </tr>
-                      )}
-                    </Draggable>
-                  ))}
-                  {droppableProvider.placeholder}
-                </tbody>
-              )}
-            </Droppable>
-          </table>
-        </DragDropContext>
+
+      <div className="button-container">
+        <button
+          className="btn btn-primary btn-md justify-content-center rounded-pill"
+          type="button"
+          onClick={() => removeFirstPokemon()}
+        >
+          Atender siguiente turno
+        </button>
       </div>
-    </div>
     </>
   );
 };
